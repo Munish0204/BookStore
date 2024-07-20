f558c7bc8691f8cd83731fa40e3446eebc7d9a8f
Muneeswaran <munishwaran933@gmail.com>
2024-07-20 17:08:04 +0530

    Add files via upload

---
+++ chatSchema.js
@@ -0,0 +1,13 @@
+
+const mongoose = require("mongoose");
+
+const Chat = new mongoose.Schema({
+   usersID: {
+      type: Array,
+      require: true
+   },
+   chat: {
+      type: Array
+   }
+});
+module.exports = mongoose.model("Chat", Chat);
