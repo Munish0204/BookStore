9ae2cacf59b251c59202d4b562aced86b6bdddb8
Muneeswaran <munishwaran933@gmail.com>
2024-07-20 17:11:20 +0530

    Add files via upload

--- index.js
+++ index.js
@@ -1,36 +1,26 @@
-const express = require("express");
-const app = express();
-const path = require("path");
-const bodyParser = require("body-parser");
-const mongoose = require("mongoose");
-const dotenv = require("dotenv");
 
-dotenv.config();
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.scss";
+import BookStore from "./BookStore";
+import * as serviceWorker from "./serviceWorker";
+import { BrowserRouter as Router } from "react-router-dom";
+import { StoreProvider } from "./Components/Store";
+import { AuthProvider } from "./Components/context/Auth";
+// if (
+//    process.env.NODE_ENV === "production"
+// ) {
+//    window.__REACT_DEVTOOLS_GLOBAL_HOOK__._renderers = {};
+// }
 
-const port = process.env.PORT || 5500;
-//Import routes
-
-mongoose.set("useFindAndModify", false);
-mongoose.connect(process.env.DB_CONNECT, { useNewUrlParser: true }, () => console.log("DB connected"));
-const handleUsers = require("./routes/handleUsers");
-const handleProducts = require("./routes/handleProducts");
-const addNewProduct = require("./routes/addNewProduct");
-
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(bodyParser.json());
-app.use("/images", express.static("images"));
-app.use(express.static(path.join(__dirname, "client/build")));
-// app.use(express.static(path.join(__dirname, "public")));
-
-app.use("/", handleProducts);
-app.use("/user", handleUsers);
-app.use("/", addNewProduct);
-app.get("/*", function(req, res) {
-   res.sendFile(path.join(__dirname, "client/build/index.html"), function(err) {
-      if (err) {
-         res.status(500).send(err);
-      }
-   });
-});
-
-app.listen(port, () => console.log(`Server started on port ${port}`));
+ReactDOM.render(
+  <StoreProvider>
+    <AuthProvider>
+      <Router>
+        <BookStore />
+      </Router>
+    </AuthProvider>
+  </StoreProvider>,
+  document.getElementById("root")
+);
+serviceWorker.unregister();
